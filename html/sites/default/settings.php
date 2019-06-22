fed101e8e5cb38bf9d41107edb382940d9f7258a
Truls S. Yggeseth <truls@signwo.com>
2019-06-22 13:59:13 +0200

    Update DDEV config

--- html/sites/default/settings.php
+++ html/sites/default/settings.php
@@ -775,3 +775,8 @@ if (getenv('LANDO_INFO')) {
 
 $settings['install_profile'] = 'social';
 $config_directories['sync'] = '../config';
+
+// Automatically generated include for settings managed by ddev.
+if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php')) {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}
